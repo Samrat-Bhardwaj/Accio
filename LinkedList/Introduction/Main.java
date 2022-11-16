7f9269e7ddc8aae0cb020f18d468ca3c1ec453d0
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-11-16 22:34:08 +0530

    dry run

--- LinkedList/Introduction/Main.java
+++ LinkedList/Introduction/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Node {
     int data;
     Node next;
@@ -12,27 +14,34 @@ public class Main {
     static Node tail;
 
     public static void addLast(int data){
-        Node nn = new Node(data);
-        tail.next = nn;
+        Node nn = new Node(data); 
 
-        tail = nn;
+        if(head == null){ // in case of 0 size ll
+            head = nn; // this is my first node of list 
+            tail = nn; // and also my last node of list 
+        } else {
+            tail.next = nn; // attaching
+
+            tail = nn; // tail = tail.next // shifting tail to last => 
+        }
     }
 
     public static void main(String[] args) {
-        Node f = new Node(7); 
-        Node s = new Node(0);
+        head =null;
+        tail = null;
 
-        Node t = new Node(1);
+        Scanner scn = new Scanner(System.in);
+        System.out.println("What will be the initial size of your list");
+        int n=scn.nextInt();
 
-        // connecting first and second
-        f.next = s;
-        s.next = t;
+        System.out.println("Enter the data elements for input");
 
-        head = f;
-        tail = t;
-
-        addLast(10);
-        addLast(59);
-        System.out.println(f.next.next.next.next.data); //59
+        for(int i=0; i<n; i++){
+            int a = scn.nextInt();
+            addLast(a);
+        }
+        
+        System.out.println(head.data);
+        System.out.println(tail.data);
     }
 }
