e0b93f6eabc60416b092f2d8300a215f5d364d5f
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-11-18 22:50:30 +0530

    LL class 3

--- LinkedList/Questions/Main.java
+++ LinkedList/Questions/Main.java
@@ -36,6 +36,10 @@ public class Main {
     }
     // approach 2
     public ListNode middleNode(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        
         ListNode slow = head;
         ListNode fast = head;
         
@@ -46,6 +50,7 @@ public class Main {
         
         return slow;
     }
+
 
     // if first middle is required (in case of even size LL) ===================
     public ListNode middleNode_(ListNode head) {
@@ -59,6 +64,56 @@ public class Main {
         
         return slow;
     }
+
+    // leetcode 141 =======================================================
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    // leetcode 142 ==============================
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                break;
+            }
+        }
+
+        if(slow != fast){ // there is no cycle 
+            return null;
+        }
+
+        // move slow back to head;
+        slow=head;
+
+        // keep moving at same pace
+        while(slow!=fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow; 
+    }
+
+    // homework => https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
     public static void main(String[] args) {
         
     }
