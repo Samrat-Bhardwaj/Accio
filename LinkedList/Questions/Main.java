4f16823897f316f9ab74991609091ef44f9b4667
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-11-22 23:05:02 +0530

    Linked List more questions

--- LinkedList/Questions/Main.java
+++ LinkedList/Questions/Main.java
@@ -94,6 +94,10 @@ public class Main {
 
     // leetcode 142 ==============================
     public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next==null){
+            return null;
+        }
+
         ListNode slow = head;
         ListNode fast = head;
 
@@ -169,6 +173,7 @@ public class Main {
         
         return head;
     }
+    
 
     // merge 2 sorted list 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2){
@@ -241,6 +246,7 @@ public class Main {
         return dummy.next;
     }
 
+    // leetcode 143 (This was asked in google) ========================================================
     public void reorderList(ListNode head) {
         // find mid
         ListNode mid = middleNode_(head);
@@ -254,6 +260,138 @@ public class Main {
 
         // merge two lists 
         head = merge(head,head2);
+    }
+
+    // leetcode 237 ========================================================
+    public void deleteNode(ListNode node) {
+        // node.val = node.next.val;
+        // node.next = node.next.next;
+
+        // copying data of the next node to myself
+        ListNode nextOfNode = node.next;
+        node.val = nextOfNode.val;
+        
+        // removing node.next
+        ListNode nextOfNextOfNode = nextOfNode.next;
+        node.next = nextOfNextOfNode;
+    }
+
+    // leetcode 160 approach 1
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curr = headA;
+        
+        while(curr.next!=null){
+            curr = curr.next;
+        }
+        
+        curr.next = headA;
+        ListNode ans = detectCycle(headB);
+        
+        curr.next=null;
+        return ans;
+    }
+
+    // leetcode 160 approach 2
+    public int getLength(ListNode head){
+        int len=0;
+        ListNode curr = head;
+        
+        while(curr!=null){
+            curr=curr.next;
+            len++;
+        }
+        
+        return len;
+    }
+    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int size1 = getLength(headA);
+        int size2 = getLength(headB);
+        
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        
+        if(size1 > size2){
+            int diff = size1- size2;
+            // shifting p1 
+            while(diff>0 && p1!=null){
+                p1=p1.next;
+                diff--;
+            }
+        } else {
+            int diff = size2 - size1;
+             // shifting p2
+            while(diff>0 && p2!=null){
+                p2=p2.next;
+                diff--;
+            }
+        }
+        
+        while(p1!=null && p2!=null){
+            if(p1==p2) return p1;
+            p1=p1.next;
+            p2=p2.next;
+        }
+        
+        return null;
+    }
+
+    // https://practice.geeksforgeeks.org/problems/given-a-linked-list-reverse-alternate-nodes-and-append-at-the-end/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+    public static Node reverseList_(Node head) {
+        Node prev = null;
+        Node curr = head;
+        
+        while(curr!=null){
+            // save next pointer 
+            Node nextOfCurr = curr.next;
+            
+            // reverse pointer
+            curr.next = prev;
+            
+            // move pointers to the next 2 nodes
+            prev = curr;
+            curr = nextOfCurr;
+        }
+        // head is now at prev
+        head = prev;
+        
+        return head;
+    }
+    
+    public static void rearrange(Node head)
+    {
+        int count = 0;
+        
+        Node dummy1 = new Node(-1);
+        Node c1 = dummy1;
+        Node dummy2 = new Node(-1);
+        Node c2 = dummy2;
+        
+        Node p = head;
+        
+        while(p!=null){
+            Node nextOfP = p.next;
+            p.next = null;
+            
+            if(count%2 == 0){
+                c1.next=p;
+                p=nextOfP;
+                c1=c1.next;
+            } else {
+                c2.next=p;
+                p=nextOfP;
+                c2=c2.next;
+            }
+            count++;
+        }
+        
+        Node head1 = dummy1.next;
+        Node head2 = dummy2.next;
+        
+        head2 = reverseList_(head2);
+        c1.next = head2;
+        
+        head=head1;
     }
     public static void main(String[] args) {
         
