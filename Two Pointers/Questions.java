6e7aa67f5d712eabf1b40477fa3f011340048cb5
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-11-24 23:06:51 +0530

    2 pointer end

--- Two Pointers/Questions.java
+++ Two Pointers/Questions.java
@@ -88,6 +88,80 @@ public class Questions {
         return ans;
     }
 
+    // https://practice.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+    void segregate0and1(int[] arr, int n) {
+        int zeros = 0;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                zeros++;
+            }
+        }
+        
+        
+        int i=0;
+        while(i<zeros){
+            arr[i]=0;
+            i++;
+        }
+        
+        while(i<n){
+            arr[i]=1;
+            i++;
+        }
+    }
+
+    // in one iteration using areas 
+
+    // {0,i-1} => all the zeros
+    // {i,j} => undiscovered
+    // {j+1,n-1} => all the ones
+    void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    void segregate0and1(int[] arr, int n) {
+        int i=0;
+        int j=n-1;
+        
+        while(i<=j){
+            if(arr[i]==1){
+                swap(arr,i,j);
+                j--;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    // leetcode 75 ===================================================
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        
+        int p1 = -1;
+        int p2 = 0;
+        int p3 = n-1;
+        
+        while(p2<=p3){ // until undiscovered area is not finish
+            if(nums[p2]==2){
+                swap(nums,p2,p3);
+                p3--;
+            } else if(nums[p2]==0){
+                p1++;
+                swap(nums,p1,p2);
+                p2++;
+            } else {
+                p2++;
+            }
+        }
+    }
+
+    // homework
+    
+    // https://course.acciojob.com/idle?question=0579040b-f013-4c85-988a-07130a995397
+    // move zeros to the end from leetcode 
     public static void main(String[] args) {
         
     }
