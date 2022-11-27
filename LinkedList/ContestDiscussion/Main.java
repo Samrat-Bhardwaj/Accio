c34163a1663cca26584ac13429d71dfe4fab4aac
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-11-27 21:03:58 +0530

    Linked list contest discussion

--- LinkedList/ContestDiscussion/Main.java
+++ LinkedList/ContestDiscussion/Main.java
@@ -10,6 +10,14 @@ public class Main {
         
     }
 
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
     // https://course.acciojob.com/idle?question=0e70b77f-c4b9-4500-b160-5b1c36fe5d9d
     Node deleteMid(Node head){
 		if(head==null){
@@ -119,6 +127,82 @@ public class Main {
 		return oddHead;
 	}
 
+    // leetcode 25 ==========================================================
+    class Solution {
+        ListNode oh;
+        ListNode ot;
+        
+        ListNode th;
+        ListNode tt;
+        
+        void addFirst(ListNode nn){
+            if(th == null){
+                th = nn;
+                tt = nn;
+            } else {
+                nn.next = th;
+                th = nn;
+            }
+        }
+        
+        int getSize(ListNode head){
+            ListNode curr = head;
+            
+            int ans = 0;
+            while(curr!=null){
+                ans++;
+                curr=curr.next;
+            }
+            
+            return ans;
+        }
+        
+        
+        public ListNode reverseKGroup(ListNode head, int k) {
+            oh = null;
+            ot = null;
+            
+            th = null;
+            tt = null;
+            
+            int size = getSize(head);
+            ListNode curr = head;
+            // base case ()
+            if(size < k){
+                return head;
+            }
+            
+            while(size>=k){
+                // reverse in pair of k
+                int K=k;
+                while(K-- > 0){
+                    ListNode nextOfCurr = curr.next;
+                    curr.next = null;
+                    
+                    addFirst(curr);
+                    curr = nextOfCurr;
+                    
+                    size--;
+                }
+                
+                // add th,tt to my original
+                if(oh==null){
+                    oh = th;
+                    ot = tt;
+                } else {
+                    ot.next = th;
+                    ot = tt;
+                }
+                
+                th = null;
+                tt = null;
+            }
+            
+            ot.next = curr;
+            
+            return oh;
+        }
+    }
     // contest question 3 (https://course.acciojob.com/idle?question=5a341a6c-8fcb-4945-bf2f-ded75cc6402a) ========================
     public static void main(String[] args) {
         
