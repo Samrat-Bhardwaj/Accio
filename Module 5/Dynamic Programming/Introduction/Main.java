b53202471bb29cca99fd2b7cc2cfd9bd9569dac6
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-12-15 21:18:56 +0530

    DP start

---
+++ Module 5/Dynamic Programming/Introduction/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static int fib_rec(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+
+        int ans = fib_rec(n-1) + fib_rec(n-2);
+
+        return ans;
+    }
+
+    public static int fib_memo(int n, int[] memo){
+        if(n==0 || n==1){
+            return memo[n]=n;
+        }
+
+        if(memo[n]!=0) return memo[n];
+
+        int ans = fib_memo(n-1,memo) + fib_memo(n-2,memo);
+
+        return memo[n]=ans;
+    }
+
+    public static int tabulation(int N, int[] memo){
+        for(int n=0; n<=N; n++){
+            if(n==0 || n==1){
+                memo[n]=n;
+                continue;
+            }
+    
+            // if(memo[n]!=0) return memo[n];
+    
+            int ans = memo[n-1] + memo[n-2]; //fib_memo(n-1,memo) + fib_memo(n-2,memo);
+    
+            memo[n]=ans;
+        }
+
+        return memo[N];
+    }
+
+    public static int fib_most_optimized(int n){
+        int f=0;
+        int s=1;
+        int t = f + s;
+
+        for(int i=2; i<=n; i++){
+            t = f + s;
+            f = s;
+            s = t;
+        }
+
+        return t;
+    }
+    public static void main(String[] args) {
+        int n = 7;
+
+        int[] memo = new int[n+1];
+        int ans = fib_memo(n,memo);
+        System.out.println(ans);
+    }    
+}
