a96585922276d4041e85647ad021607de806d4be
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2022-12-23 21:58:36 +0530

    Wildcard matching

--- Module 5/Dynamic Programming/String set/Main.java
+++ Module 5/Dynamic Programming/String set/Main.java
@@ -392,6 +392,138 @@ public class Main {
 
     // homework =========================================================
     // https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+    
+    // https://course.acciojob.com/idle?question=eaebd540-75c3-4284-91de-5c798a8f4bf5
+    public static int fun(String s)
+    {
+        long ewa = 0;
+        long ewb = 0;
+        long ewc = 0;
+
+		long mod=(long)1e9+7;
+
+		for(int i=0; i<s.length(); i++){
+			if(s.charAt(i)=='a'){
+				ewa = 2*ewa + 1;
+			} else if(s.charAt(i)=='b'){
+				ewb = 2*ewb + ewa;
+			} else if(s.charAt(i)=='c'){
+				ewc = 2*ewc + ewb;
+			}
+
+			ewa = ewa%mod;
+			ewb = ewb%mod;
+			ewc = ewc%mod;
+		}
+
+		return (int)(ewc%mod);
+    }
+
+    // leetcode 44 ====================================================================================
+
+    public boolean wildCard_rec(int n, int m, String s, String p){
+        if(n==0 || m==0){
+            if(n==0 && m==0){
+                return true;
+            } else if(m==1 && p.charAt(m-1)=='*'){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if(p.charAt(m-1)=='?'){
+            return wildCard_rec(n-1, m-1, s, p);
+        } else if(p.charAt(m-1)=='*'){
+            return wildCard_rec(n-1, m, s, p) || wildCard_rec(n, m-1, s, p);
+        } else if(s.charAt(n-1) == p.charAt(m-1)){
+            return wildCard_rec(n-1, m-1, s, p);
+        } else {
+            return false;
+        }
+    }
+
+    public int wildCard_memo(int n, int m, String s, String p,int[][] dp){
+        if(n==0 || m==0){
+            if(n==0 && m==0){
+                return dp[n][m] = 1;
+            } else if(m==1 && p.charAt(m-1)=='*'){
+                return dp[n][m] = 1;
+            } else {
+                return dp[n][m] = 0;
+            }
+        }
+
+        if(dp[n][m]!=-1) return dp[n][m];
+
+        if(p.charAt(m-1)=='?'){
+            return dp[n][m] = wildCard_memo(n-1, m-1, s, p,dp);
+        } else if(p.charAt(m-1)=='*'){
+            return dp[n][m] = wildCard_memo(n-1, m, s, p,dp) | wildCard_memo(n, m-1, s, p,dp);
+        } else if(s.charAt(n-1) == p.charAt(m-1)){
+            return dp[n][m] = wildCard_memo(n-1, m-1, s, p,dp);
+        } else {
+            return dp[n][m] = 0;
+        }
+    }
+
+    public int wildCard_tab(int N, int M, String s, String p, int[][] dp){
+        for(int n=0; n<=N; n++){
+            for(int m=0; m<=M; m++){
+                if(n==0 || m==0){
+                    if(n==0 && m==0){
+                        dp[n][m] = 1;
+                    } else if(m==1 && p.charAt(m-1)=='*'){
+                        dp[n][m] = 1;
+                    } else {
+                        dp[n][m] = 0;
+                    }
+                    continue;
+                }
+        
+                if(p.charAt(m-1)=='?'){
+                    dp[n][m] = dp[n-1][m-1]; //wildCard_memo(n-1, m-1, s, p,dp);
+                } else if(p.charAt(m-1)=='*'){
+                    dp[n][m] = dp[n-1][m] | dp[n][m-1]; //wildCard_memo(n-1, m, s, p,dp) | wildCard_memo(n, m-1, s, p,dp);
+                } else if(s.charAt(n-1) == p.charAt(m-1)){
+                    dp[n][m] = dp[n-1][m-1]; //wildCard_memo(n-1, m-1, s, p,dp);
+                } else {
+                    dp[n][m] = 0;
+                }
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        int[][] dp = new int[n+1][m+1];
+
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<m;){
+            if(p.charAt(i)=='*'){
+                sb.append("*");
+                while(i<m && p.charAt(i)=='*') i++;
+            } else {
+                sb.append(p.charAt(i));
+                i++;
+            }
+        }
+
+        p=sb.toString();
+        m=p.length();
+
+        return wildCard_tab(n, m, s, p, dp) == 1 ? true : false; 
+        // return wildCard_memo(n,m,s,p,dp) == 1 ? true : false;
+        // return wildCard_rec(n,m,s,p);
+    }
+
     public static void main(String[] args) {
         
     }
