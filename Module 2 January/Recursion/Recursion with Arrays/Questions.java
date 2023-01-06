781cbc56ffe9c5f5635627c83d8ca24586ff7499
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2023-01-06 22:09:43 +0530

    First last index

--- Module 2 January/Recursion/Recursion with Arrays/Questions.java
+++ Module 2 January/Recursion/Recursion with Arrays/Questions.java
@@ -16,8 +16,57 @@ public class Questions {
         printReverse(arr, idx+1);
         System.out.println(arr[idx]);
     }
+
+    public static int maxofArray(int[] arr, int idx){
+        if(idx == arr.length){
+            return Integer.MIN_VALUE;
+        }
+
+        int smallAns = maxofArray(arr, idx+1);
+
+        int ans = Math.max(arr[idx],smallAns);
+        return ans;
+    }
+
+    public static int firstIndex(int[] arr, int idx, int tar){
+        if(idx==arr.length){
+            return -1;
+        }
+
+        int smallAns = firstIndex(arr, idx+1, tar);
+        int ans = 0;
+
+        if(arr[idx] == tar){
+            ans = idx;
+        } else {
+            ans = smallAns;
+        }
+
+        return ans;
+    }
+
+    public static int lastIndex(int[] arr, int idx, int tar){
+        if(idx == arr.length){
+            return -1;
+        }
+        
+        int smallAns = lastIndex(arr, idx+1, tar); 
+
+        int ans = 0;
+        if(smallAns == -1){
+            if(arr[idx]==tar){
+                ans = idx;
+            } else {
+                ans = smallAns;
+            }
+        } else {
+            ans = smallAns;
+        }
+
+        return ans;
+    }
     public static void main(String[] args) {
         int[] arr = {10,9,4,11,20};
-        printReverse(arr,0);
+        System.out.println(maxofArray(arr,0));
     }
 }
