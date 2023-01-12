82656969d45675b33a4b76a65647612cd32cb310
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2023-01-12 23:41:25 +0530

    Recursion end

--- Module 2 January/Recursion/Recursion With Strings (Print Types)/Main.java
+++ Module 2 January/Recursion/Recursion With Strings (Print Types)/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void printSubs(String str, String asf){
         if(str.length()==0){
@@ -75,6 +77,107 @@ public class Main {
         }
     }
 
+    // print permutations ========================================
+    public static void permutationPrint(String ques, String asf) {
+        if(ques.length()==0){
+            System.out.println(asf);
+            return;
+        }
+
+        for(int i=0; i<ques.length(); i++){
+            char toAdd = ques.charAt(i);
+
+            String leftPart = ques.substring(0, i);
+            String rightPart = ques.substring(i+1, ques.length());
+
+            String roq = leftPart + rightPart;
+            permutationPrint(roq, asf+toAdd);
+        }
+    }
+
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+        if(sr == dr && sc == dc){
+            System.out.println(psf);
+            return;
+        }
+
+        // if(sr>dr || sc>dc){
+        //     return;
+        // }
+
+        // horizontal steps 
+        for(int jump = 1; jump<=dc-sc; jump++){
+            printMazePaths(sr, sc + jump, dr, dc, psf + "h"+ jump);
+        }
+
+        // vertical steps
+        for(int jump = 1; jump<=dr-sr; jump++){
+            printMazePaths(sr+jump, sc, dr, dc, psf+"v"+jump);
+        }
+
+        // diagonal steps
+        for(int jump=1; jump<=dc-sc && jump<=dr-sr; jump++){
+            printMazePaths(sr + jump, sc + jump, dr, dc, psf+"d"+jump);
+        }
+    }
+
+
+    // recursion on the way down =============================================
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc){
+        if(sr==dr && sc==dc){
+            ArrayList<String> ba = new ArrayList<>();
+            ba.add("");
+            return ba;
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+        for(int jump=1; jump<=dc-sc; jump++){
+            ArrayList<String> hpaths = getMazePaths(sr, sc+jump, dr, dc);
+
+            for(int i=0; i<hpaths.size(); i++){
+                String hpath = hpaths.get(i);
+                String path = "h" + jump + hpath;
+                ans.add(path);
+            }
+        }
+
+        for(int jump=1; jump<=dr-sr; jump++){
+            ArrayList<String> vpaths = getMazePaths(sr+jump, sc, dr, dc);
+
+            for(int i=0; i<vpaths.size(); i++){
+                String vpath = vpaths.get(i);
+                String path = "v" + jump + vpath;
+                ans.add(path);
+            }
+        }
+
+        for(int jump=1; jump<=dr-sr; jump++){
+            ArrayList<String> dpaths = getMazePaths(sr+jump, sc+jump, dr, dc);
+
+            for(int i=0; i<dpaths.size(); i++){
+                String dpath = dpaths.get(i);
+                String path = "d" + jump + dpath;
+                ans.add(path);
+            }
+        }
+
+        return ans;
+    }
+
+    // number of ways to make natural number 
+    public static long ways(int sum, int ld){
+		if(sum==0){
+			return 1;
+		}
+		long ans = 0;
+
+		for(int i=ld+1; i<=sum; i++){
+			long smallAns = ways(sum - i, i);
+			ans = ans + smallAns;
+		}
+
+		return ans;
+    }
     public static void main(String[] args) {
         // printSubs("abc", "");
         // printKPC("789", "");
