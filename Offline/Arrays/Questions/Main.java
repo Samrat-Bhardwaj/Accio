0f436ed063efb9b455b9615185a22f1d70b4ebdf
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2024-03-09 17:39:28 +0530

    Arrays max

--- Offline/Arrays/Questions/Main.java
+++ Offline/Arrays/Questions/Main.java
@@ -1,6 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void ArrayOperations(int[] arr, int n) {
+         // sum, average, max 
+        int sum = 0;
+        int average = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+            sum = sum + arr[i];
+        }
+
+        average = sum/n;
+
+        // 10,4,5,6,88
+        for(int i=0; i<n; i++){
+            int ele = arr[i];
+
+            if(ele > max){
+                max = ele;
+            }
+        }
+
+        System.out.print(sum + " " + average + " " + max);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
